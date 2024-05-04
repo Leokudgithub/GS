bfdf5897556ba9642fa45f5e5b935725a4c5b08b
Leokudgithub <leokudyt@gmail.com>
2024-05-04 13:38:27 +0300

    print, var, newline, add, mult, div, sub, enable comments(//), code in files, running with "gs run file.gs", infinity command entering

--- main.cpp
+++ main.cpp
@@ -4,32 +4,39 @@
 #include <fstream>
 #include "interpreter.h"
 using namespace std;
+
 int main() {
     std::string input;
-    std::getline(std::cin, input);
-    if(input.substr(0, 6) == "gs run")
-    {
-        std::string filepath = input.substr(7);
-        std::ifstream file(filepath); // открываем файл для чтения
-        if (!file.is_open()) {
-            std::cerr << "file opening error" << std::endl;
-            return 1;
+
+    while (true) {
+        std::getline(std::cin, input);
+
+        if(input == "exit") {
+            break;
         }
 
-        std::vector<std::string> code;
-        std::string line;
-        while (std::getline(file, line)) {
-            code.push_back(line);
-        }
-        file.close(); // закрываем файл
+        if(input.substr(0, 6) == "gs run")
+        {
+            std::string filepath = input.substr(7);
+            std::ifstream file(filepath); // открываем файл для чтения
+            if (!file.is_open()) {
+                std::cerr << "file opening error" << std::endl;
+                return 1;
+            }
 
-        int line_num = 0; // индекс строки для интерпретации
-        while (line_num < code.size()) {
-            handle(code[line_num], line_num);
-            line_num++;
-        }
+            std::vector<std::string> code;
+            std::string line;
+            while (std::getline(file, line)) {
+                code.push_back(line);
+            }
+            file.close(); // закрываем файл
 
-        return 0;
+            int line_num = 0; // индекс строки для интерпретации
+            while (line_num < code.size()) {
+                handle(code[line_num], line_num);
+                line_num++;
+            }
+        }
     }
-
+    return 0;
 }
