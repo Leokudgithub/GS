f67e22bb82112a7f0393525856efb0c56deba16c
Leokudgithub <leokudyt@gmail.com>
2024-05-07 22:08:11 +0300

    print, int(replacing var), newline, add, mult, div, sub, enable comments(//), code in files, running with "gs run file.gs", infinity command entering, line_num in header file, made str arg const&, running file GS.exe run file.gs, add GS.exe to PATH

--- main.cpp
+++ main.cpp
@@ -2,38 +2,27 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cstring>
 #include "interpreter.h"
 using namespace std;
-
-int main(int argc, char** argv) {
-    std::string input;
-
-    while (true) {
-        std::getline(std::cin, input);
-
-        if(input == "exit") {
-            break;
+int main(int argc, char* argv[]) {
+    if(argc == 3 && strcmp(argv[1], "run") == 0)
+    {
+        std::string filepath =  argv[2];
+        std::ifstream file(filepath); // открываем файл для чтения
+        if (!file.is_open()) {
+            std::cerr << "file opening error" << std::endl;
+            return 1;
         }
-
-        if(input.substr(0, 6) == "gs run")
-        {
-            std::string filepath = input.substr(7);
-            std::ifstream file(filepath); // открываем файл для чтения
-            if (!file.is_open()) {
-                std::cerr << "file opening error" << std::endl;
-                return 1;
-            }
-
-            std::vector<std::string> code;
-            std::string line;
-            while (std::getline(file, line)) {
-                code.push_back(line);
-            }
-            file.close(); // закрываем файл
-            while (line_num < code.size()) {
-                handle(code[line_num], line_num);
-                line_num++;
-            }
+        std::vector<std::string> code;
+        std::string line;
+        while (std::getline(file, line)) {
+            code.push_back(line);
+        }
+        file.close(); // закрываем файл
+        while (line_num < code.size()) {
+            handle(code[line_num], line_num);
+            line_num++;
         }
     }
     return 0;
